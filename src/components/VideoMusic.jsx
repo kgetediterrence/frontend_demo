4774806d59fe47798ac42b57184b02370432bc61
thambosifiso <ThamboS@bathinibona.co.za>
2025-12-19 07:03:33 +0200

    Update VideoMusic.jsx

--- src/components/VideoMusic.jsx
+++ src/components/VideoMusic.jsx
@@ -6,7 +6,7 @@ function VideoMusic() {
       <h2>Welcome to Video and Music Dashboard</h2>
       <p>Browse videos and music</p>
       <input type="text" placeholder="Search" />
-      <button>Add to Cart</button>
+      <button>Add to list</button>
     </div>
   );
 }
